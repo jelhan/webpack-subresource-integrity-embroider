df28d4d9b8f72121cc99e845fc86750c784a639e
Jeldrik Hanschke <admin@jhanschke.de>
2024-02-17 17:00:04 +0100

    setup infra for multiple test apps

--- tests/test.js
+++ tests/test.js
@@ -1,51 +1,61 @@
 import { expect } from "chai";
-import { readFileSync } from "node:fs";
+import { readdirSync, readFileSync } from "node:fs";
+import path from 'node:path';
 import { JSDOM } from "jsdom";
+import { fileURLToPath } from 'url';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 describe("Embroider build", function () {
   let indexHtml;
 
-  before(function () {
-    try {
-      const indexHtmlContent = readFileSync("../test-app/dist/index.html", {
-        encoding: "utf8",
+  const scenarios = readdirSync(path.join(__dirname, '..', 'test-apps'));
+
+  for (const scneario of scenarios) {
+    describe(`Scenario: ${scneario}`, function() {
+      before(function () {
+        try {
+          const indexHtmlContent = readFileSync(path.join('..', 'test-apps', scneario, 'dist', 'index.html'), {
+            encoding: "utf8",
+          });
+          indexHtml = new JSDOM(indexHtmlContent).window.document;
+        } catch (error) {
+          throw new Error(
+            "Parsing index.html of test-app build failed. Double check that test-app has been build successfuly.",
+            { cause: error },
+          );
+        }
       });
-      indexHtml = new JSDOM(indexHtmlContent).window.document;
-    } catch (error) {
-      throw new Error(
-        "Parsing index.html of test-app build failed. Double check that test-app has been build successfuly.",
-        { cause: error },
-      );
-    }
-  });
 
-  it("adds integrity attribute to link elements for stylesheets", function () {
-    for (const el of indexHtml.querySelectorAll("link")) {
-      expect(el.hasAttribute("integrity")).to.be.true;
-      expect(el.getAttribute("integrity")).to.be.a("string");
-    }
-  });
+      it("adds integrity attribute to link elements for stylesheets", function () {
+        for (const el of indexHtml.querySelectorAll("link")) {
+          expect(el.hasAttribute("integrity")).to.be.true;
+          expect(el.getAttribute("integrity")).to.be.a("string");
+        }
+      });
 
-  it("adds integrity attribute to script elements", function () {
-    for (const el of indexHtml.querySelectorAll("script")) {
-      expect(el.hasAttribute("integrity")).to.be.true;
-      expect(el.getAttribute("integrity")).to.be.a("string");
-    }
-  });
+      it("adds integrity attribute to script elements", function () {
+        for (const el of indexHtml.querySelectorAll("script")) {
+          expect(el.hasAttribute("integrity")).to.be.true;
+          expect(el.getAttribute("integrity")).to.be.a("string");
+        }
+      });
 
-  it("adds crossorigin attribute to link elements for stylesheets", function () {
-    for (const el of indexHtml.querySelectorAll("script")) {
-      expect(el.hasAttribute("crossorigin")).to.be.true;
-      expect(el.getAttribute("crossorigin")).to.be.a("string");
-      expect(el.getAttribute("crossorigin")).to.equal("anonymous");
-    }
-  });
+      it("adds crossorigin attribute to link elements for stylesheets", function () {
+        for (const el of indexHtml.querySelectorAll("script")) {
+          expect(el.hasAttribute("crossorigin")).to.be.true;
+          expect(el.getAttribute("crossorigin")).to.be.a("string");
+          expect(el.getAttribute("crossorigin")).to.equal("anonymous");
+        }
+      });
 
-  it("adds crossorigin attribute to script elements", function () {
-    for (const el of indexHtml.querySelectorAll("script")) {
-      expect(el.hasAttribute("crossorigin")).to.be.true;
-      expect(el.getAttribute("crossorigin")).to.be.a("string");
-      expect(el.getAttribute("crossorigin")).to.equal("anonymous");
-    }
-  });
+      it("adds crossorigin attribute to script elements", function () {
+        for (const el of indexHtml.querySelectorAll("script")) {
+          expect(el.hasAttribute("crossorigin")).to.be.true;
+          expect(el.getAttribute("crossorigin")).to.be.a("string");
+          expect(el.getAttribute("crossorigin")).to.equal("anonymous");
+        }
+      });
+    });
+  }
 });
